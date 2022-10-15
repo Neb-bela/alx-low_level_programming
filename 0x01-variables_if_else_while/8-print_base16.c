e9c971cc209492ce738aaf6df0911f090435e071
nebiyat <nebibela.91@gmail.com>
2022-10-14 18:12:27 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main- Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 17; i++)
+	{
+		putchar(0xi);
+	}
+	putchar('\n');
+	return (0);
+}
