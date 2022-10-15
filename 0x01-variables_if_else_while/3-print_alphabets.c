923e5c4ade3466396bdb8c69aeb12dd1bbdcccfa
nebiyat <nebibela.91@gmail.com>
2022-10-14 17:17:03 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+	for (i = 0; i < 52; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
