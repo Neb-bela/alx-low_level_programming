c69c2ae1347fd2bb411e0edc0d7560bd5143cd29
nebiyat <nebibela.91@gmail.com>
2022-10-15 09:07:45 -0700

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+/**
+ * main- Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 48; i < 57; i++)
+	{
+		for (j = i + 1; j <= 57; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				putchar(k);
+				putchar(i);
+				putchar(32);
+				putchar(k);
+				putchar(j);
+			}
+			if (i == 56)
+			{
+				putchar(10);
+			}
+			else
+			{
+				putchar(44);
+			}
+		}
+	}
+	return (0);
+}
