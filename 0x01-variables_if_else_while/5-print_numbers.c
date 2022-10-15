f812ae64531b9f9802d162a2e9ce709f794f8b79
nebiyat <nebibela.91@gmail.com>
2022-10-14 17:30:20 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- Entry
+ * Return: Always 0
+ */
+int main(void)
+{
+	char num[10] = "0123456789";
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(num[i]);
+	}
+	putchar('\n');
+	return (0);
+}
