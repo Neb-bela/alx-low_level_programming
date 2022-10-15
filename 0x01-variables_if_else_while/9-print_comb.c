01c57be8fdb4bd07d7f60051403020daccb86233
nebiyat <nebibela.91@gmail.com>
2022-10-14 19:50:44 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,12 @@ int main(void)
 {
 	int i;
 
-	for (i = 48; i < 58; i++)
+	for (i = 48; i < 57; i++)
 	{
 		putchar(i);
 		putchar(44);
 		putchar(32);
 	}
-	putchar('\n');
+	putchar(58);
 	return (0);
 }
