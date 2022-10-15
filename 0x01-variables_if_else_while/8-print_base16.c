f8a9a6ba2e7afc0b54196a47670130418b3619db
nebiyat <nebibela.91@gmail.com>
2022-10-14 18:20:16 -0700

    8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -7,9 +7,13 @@ int main(void)
 {
 	int i;
 
-	for (i = 0; i < 17; i++)
+	for (i = 48; i < 58; i++)
 	{
-		putchar("%x", i);
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
+	{
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
