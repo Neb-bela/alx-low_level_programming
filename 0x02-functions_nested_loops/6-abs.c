a4d42c4fd8768f12bf46c5b489150a11b0d4ac34
nebiyat <nebibela.91@gmail.com>
2022-10-18 14:27:04 -0700

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *_abs- checks if lowercase
+ *@int: integer
+ * Return: Always 0
+ */
+int _abs(int)
+{
+	int c;
+	int d;
+
+	if (c >= 0)
+		printf("%d\n", c);
+	else
+	{
+		d = -1 x c;
+		printf("%d\n", d);
+	}
+	return (0);
+}
