6c7685dcf8842dec514e3ededf5313b34531de5b
nebiyat <nebibela.91@gmail.com>
2022-10-18 12:44:15 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ *print_alphabet_x10- writes characters
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+		char la;
+		int i;
+
+		for (i = 0; i < 10; i++)
+		{
+			for (la = 'a'; la <= 'z'; la++)
+			{
+				_putchar(la);
+			}
+		}
+			_putchar('\n');
+}
