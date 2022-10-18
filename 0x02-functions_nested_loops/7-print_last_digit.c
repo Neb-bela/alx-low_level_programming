39640ccf462f45e89190c724f15558389e23f349
nebiyat <nebibela.91@gmail.com>
2022-10-18 14:47:20 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *print_last_digit- checks if lowercase
+ *@a: integer
+ *@b: integer module
+ * Return: Always 0
+ */
+int print_last_digit(int a, b)
+{
+	b = a % 10;
+	return (b);
+}
