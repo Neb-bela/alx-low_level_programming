991375391c09af395fb69fd4f2ce3845209fd9d9
nebiyat <nebibela.91@gmail.com>
2022-10-20 12:11:53 -0700

    0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-
-
 /**
  *
  * * main - tests function that prints if integer is positive or negative
@@ -10,21 +8,13 @@
  *
  * */
 
-
-
 int main(void)
 
 {
+	int i;
 
-	        int i;
-
-
-
-		        i = 98;
-
-			        positive_or_negative(i);
-
-
-
-				        return (0);
+	i = 98;
+       	positive_or_negative(i);
+	
+	return (0);
 }
