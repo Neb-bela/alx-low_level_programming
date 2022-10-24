816d33f9521829aeb761784f619684b51f3de8bf
nebiyat <nebibela.91@gmail.com>
2022-10-24 13:19:54 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *print_rev- pointer
+ *@s: s pointer
+ *Return:void
+ *
+ */
+void print_rev(char *s)
+{
+	while (*s != '\0')
+	{
+		i++;
+	}
+	for (i = i - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
+}
