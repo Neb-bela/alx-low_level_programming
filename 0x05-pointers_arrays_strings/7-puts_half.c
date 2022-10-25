e20050d527245c4609117b03977088889b1054bb
nebiyat <nebibela.91@gmail.com>
2022-10-24 20:01:25 -0700

    7-puts_half.c

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
+/**
+ *puts_half- pointer
+ *@str: s pointer
+ *Return:void
+ *
+ */
+void puts_half(char *str)
+{
+	int l = 0;
+
+	while (*str != '\0')
+	{
+		l++;
+		str++;
+	}
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		str++;
+	}
+	_putchar('\n');
+}
