b2d46e1064fb761019233d6ed575da82e12e6c9b
nebiyat <nebibela.91@gmail.com>
2022-10-27 01:24:01 -0700

    5-string_toupper.c

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ *string_toupper- concatenation
+ *@char- character
+ *Return: n
+ */
+char *string_toupper(char *)
+{
+	int i;
+	int *n;
+
+	i = 0;
+	for (n[i] != '\0';)
+	{
+		if (n[i] >= a && n[i] <= 'z')
+			n[i] = n[i] - 32;
+		i++;
+	}
+	return (n);
+}
