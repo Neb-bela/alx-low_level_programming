896bffb7c9d9f650b69b3ebe188ed5510979b530
nebiyat <nebibela.91@gmail.com>
2022-11-02 18:16:21 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ *factorial- prints a string followed by a new line
+ *@n: The string to be printed
+ *Return: If n > 0the factorial of n.
+ */
+int factorial(int n)
+{
+	int result = n;
+
+	if (n < 0)
+		return (-1);
+	else if (n >= 0 && n <= 1)
+		return (1);
+}
